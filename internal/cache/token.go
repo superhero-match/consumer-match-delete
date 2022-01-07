adcd8840530ac4217e8fc8bc7d79e9987b94582d
Nikolajus Karpovas <superheromatch@gmail.com>
2022-01-07 19:00:44 +0200

    Add Cache interface to cache package

--- internal/cache/token.go
+++ internal/cache/token.go
@@ -19,7 +19,7 @@ import (
 )
 
 // GetFirebaseMessagingToken fetches choice(like, dislikes are only in DB) from cache.
-func (c *Cache) GetFirebaseMessagingToken(key string) (*model.FirebaseMessagingToken, error) {
+func (c *cache) GetFirebaseMessagingToken(key string) (*model.FirebaseMessagingToken, error) {
 	res, err := c.Redis.Get(key).Result()
 	if err != nil && err != redis.Nil {
 		return nil, err
